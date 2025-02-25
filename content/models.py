72bd36f011b6cc4538cfcf1e9dc403e58d2f4c33
Samguy1 <afolabiayoola001@gmail.com>
2025-02-25 06:30:27 +0100

    Created an endpoint to read data from... Users can now get post and filter the post whether by uuid or by an id

---
+++ content/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from common.models import AbstractModel
+from access.models import User
+
+
+class Post(AbstractModel):
+
+    class Status(models.TextChoices):
+        DRAFT = "draft", _("Draft")
+        PUBLISHED = "published", _("Published")
+        REMOVED = "removed", _("Removed")
+
+    owner = models.ForeignKey(
+        User,
+        verbose_name=_("Owner"),
+        related_name="posts",
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(_("Title"), max_length=225)
+    body = models.TextField(_("Body"))
+
+    status = models.CharField(
+        _("Status"), max_length=16, choices=Status.choices, default=Status.DRAFT
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(AbstractModel):
+
+    owner = models.ForeignKey(
+        User,
+        verbose_name=_("Owner"),
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        Post,
+        verbose_name=_("Post"),
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+
+    body = models.TextField(_("Body"))
+    is_deleted = models.BooleanField(_("Is Deleted"), default=False)
+
+    def __str__(self):
+        return self.body
